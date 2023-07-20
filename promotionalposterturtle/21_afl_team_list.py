ab6790b2751d7efc516bbd22d9aceb419f7b4d7b
rxzheng <richardxzheng@outlook.com>
2023-07-20 15:30:28 +1000

    Promotoinal Poster

---
+++ promotionalposterturtle/21_afl_team_list.py
@@ -0,0 +1,4 @@
+list = ["Brisbane Lions", "Gold Coast Suns", "Sydney Swans", "Greater Western Sydney Giants", "Adelaide Crows", "Port Adelaide Power", "West Coast Eagles", "Fremantle Dockers"]
+for i in list:
+    print(i)
+
