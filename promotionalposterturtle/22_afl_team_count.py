8a430a2da314e6853fc61f43ed5650850343074f
rxzheng <richardxzheng@outlook.com>
2023-07-21 10:31:50 +1000

    22, 23, 24

---
+++ promotionalposterturtle/22_afl_team_count.py
@@ -0,0 +1,8 @@
+list = dict({"Brisbane Lions" : "Queensland", "Gold Coast Suns" "Queensland", "Sydney Swans" : "Sydney", "Greater Western Sydney Giants" : "Sydney", "Adelaide Crows" : "South Australia", "Port Adelaide Power" : "South Austalia", "West Coast Eagles" : "Western Australia", "Fremantle Dockers" : "Western Austalia"})
+print list
+count = 0
+for i in list:
+    if :
+        count += 1
+print("There are " + str(count) + " teams in the AFL that are not based in Victoria")
+
---
+++ promotionalposterturtle/23_afl_team_search.py
@@ -0,0 +1,4 @@
+list = ["Brisbane Lions", "Gold Coast Suns", "Sydney Swans", "Greater Western Sydney Giants", "Adelaide Crows", "Port Adelaide Power", "West Coast Eagles", "Fremantle Dockers"]
+team = input("What team would you like to search for in the non-Victorian AFL team list: ")
+if team in list:
+    print ("Yes")
