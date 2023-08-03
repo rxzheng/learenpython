b11b51a1a39fab50dc22598dc7d1a1a3b5e0a8eb
rxzheng <richardxzheng@outlook.com>
2023-08-03 14:49:16 +1000

    Did squares.py

--- 6_squares2.py
+++ 6_squares2.py
@@ -1,15 +1,19 @@
 from turtle import *
+
 penup()
 pensize(10)
 color("green")
 goto(-365, 300)
 pendown()
 setheading(180)
-def square:
-    for i in range (4):
-    forward(100)
-    right(90)
+
+def square():
+    for i in range(4):
+        forward(100)
+        right(90)
+
 square()
+
 penup()
 color("red")
 goto(265, 200)
