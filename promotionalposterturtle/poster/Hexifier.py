1e3187f3d437e8941b77a5590bc8ccdc2cdd2407
rxzheng <richardxzheng@outlook.com>
2023-10-24 20:43:18 +1100

    t

--- promotionalposterturtle/poster/Hexifier.py
+++ promotionalposterturtle/poster/Hexifier.py
@@ -1,29 +1,29 @@
 from PIL import Image
-
+import sys
+sys.stdout = open("hexifier.txt", "w")
 # REPLACE IMGPATH WITH THE IMAGE'S PATH
 img = Image.open('/Users/richardzheng/Documents/GitHub/learnpython/promotionalposterturtle/poster/hangingRockImage.jpg')
 
-width, height = img.size
-print("Image Width:", width)
-print("Image Height:", height)
 #RGB
-#rgb_img = img.convert('RGB')
+rgb_img = img.convert('RGB')
 
 # Get the width and height of the image
-#width, height = rgb_img.size
-
+width, height = rgb_img.size
+print(str(width)+" "+str(height))
 
 hexl = []
 # LOOP THROUgH ALL PIXELS
-#for y in range(height):
- #   for x in range(width):
+for y in range(height):
+    for x in range(width):
         #RGB of every pixel
-    #    r, g, b = rgb_img.getpixel((x, y))
+        r, g, b = rgb_img.getpixel((x, y))
 
         #RGB to HEX
-#        hex_code = '#{0:02x}{1:02x}{2:02x}'.format(r, g, b)
+        hex_code = '#{0:02x}{1:02x}{2:02x}'.format(r, g, b)
 
         # Print the hex code
- #       hexl.append(hex_code)
-#print(hexl)
-
+        hexl.append(hex_code)
+import sys
+sys.stdout = open("hexifer.txt", "w")
+for i in hexl:
+  print(i+" ", end = '')
--- promotionalposterturtle/poster/scriptv3.py
+++ promotionalposterturtle/poster/scriptv3.py
@@ -1,23 +1,23 @@
 import turtle
 import _tkinter as tk
 import sys
-sys.stdin = open("hexifer.txt", "r")
+sys.stdin = open("hexifier.txt", "r")
 width, height = input().split()
 hexl = []
 hexl = list(input().split())
 # Set up the Turtle screen
 screen = turtle.Screen()
-screen.setup(400, 400)
+screen.setup(900, 1200)
 
 # Create a Turtle object
 t = turtle.Turtle()
 t.speed(0)  # Set the drawing speed (0 is the fastest)
-
+turtle.tracer(0, 0)
 # Set the initial position for drawing
 x, y = 100, 100
 
 # Set the size of each square
-square_size = 20
+square_size = 2
 
 ptr = 0
 # Loop through the list of hex color codes and draw squares
@@ -32,12 +32,12 @@ for hex_code in hexl:
     t.end_fill()  # End filling
 
     # Move to the next position
-    x += square_size
-    ptr+=1
-    if ptr == width:
-        ptr = 0
-        y -= square_size
-        x = 100
+    if x < 100:
+        x += square_size
+    else:
+        x = -800
+        y = y + 5
+turtle.update
 
 # Close the Turtle window on click
 screen.exitonclick()
